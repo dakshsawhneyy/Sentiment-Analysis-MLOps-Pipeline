dd54e4213a281ca814cba03f05ddf354522ef7ff
Daksh Sawhney <darkstardaksh@gmail.com>
2025-11-27 10:00:20 +0530

    Pytests added for CI DevSecOps Actions

---
+++ tests/test_imports.py
@@ -0,0 +1,18 @@
+def test_import_ingestor():
+    import importlib
+    importlib.import_module("ingestor")  # or correct path
+
+
+def test_import_processor():
+    import importlib
+    importlib.import_module("processor")
+
+
+def test_import_trainer():
+    import importlib
+    importlib.import_module("trainer")
+
+
+def test_import_server():
+    import importlib
+    importlib.import_module("server")
