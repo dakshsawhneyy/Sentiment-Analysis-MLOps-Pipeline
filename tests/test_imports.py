66b5af693e2f8b2343ce50ba2d0470c41687d32a
Daksh Sawhney <darkstardaksh@gmail.com>
2025-11-28 17:51:03 +0530

    Fixed Action workflow, Fixing tests as well

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,18 +1,18 @@
 def test_import_ingestor():
     import importlib
-    importlib.import_module("ingestor")  # or correct path
+    importlib.import_module("src.ingestor.ingestor")  # or correct path
 
 
 def test_import_processor():
     import importlib
-    importlib.import_module("processor")
+    importlib.import_module("src.processor.processor")
 
 
 def test_import_trainer():
     import importlib
-    importlib.import_module("trainer")
+    importlib.import_module("src.trainer.train_baseline")
 
 
 def test_import_server():
     import importlib
-    importlib.import_module("server")
+    importlib.import_module("serving.server")
